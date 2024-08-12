6afef178306fbf47e5d806b3d91338a0a2d94c72
MoritzZ <108957845+MoeZia@users.noreply.github.com>
2024-08-12 16:20:21 +0200

    update

--- Assets/NewPlayer.cs
+++ Assets/NewPlayer.cs
@@ -6,7 +6,6 @@ using Photon.Pun;
 
 public class NewPlayer : MonoBehaviour
 {
-
     public Transform head;
     public Transform leftHand;
     public Transform rightHand;
@@ -16,26 +15,33 @@ public class NewPlayer : MonoBehaviour
     void Start()
     {
         photonView = GetComponent<PhotonView>();
+
+        // Disable the hands and head for remote players
+        if (!photonView.IsMine)
+        {
+            // These objects should remain active for the local player, but be disabled for remote players
+            rightHand.gameObject.SetActive(false);
+            leftHand.gameObject.SetActive(false);
+            head.gameObject.SetActive(false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(photonView.IsMine)
+        // Only update the transforms if this is the local player's PhotonView
+        if (photonView.IsMine)
         {
-            rightHand.gameObject.SetActive(false);
-            leftHand.gameObject.SetActive(false);
-            head.gameObject.SetActive(false);
-
+            // Map the VR devices' positions and rotations to the local player's avatar
             MapPosition(head, XRNode.Head);
             MapPosition(leftHand, XRNode.LeftHand);
             MapPosition(rightHand, XRNode.RightHand);
-
         }
     }
 
     void MapPosition(Transform target, XRNode node)
     {
+        // Get the position and rotation from the XR device and apply it to the target transform
         InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 position);
         InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.deviceRotation, out Quaternion rotation);
 
