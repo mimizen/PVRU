98e8080958bbc0441e79343eaf089058390e8590
MoeZia <108957845+MoeZia@users.noreply.github.com>
2024-08-16 15:33:22 +0200

    counter fixed

--- Assets/ScoreCounter.cs
+++ Assets/ScoreCounter.cs
@@ -11,17 +11,13 @@ public class ScoreCounter : MonoBehaviourPunCallbacks
         // Check if the collided object has the tag "checkpoint"
         if (other.CompareTag("Checkpoint"))
         {
-            // Increment the score
-            score++;
-
-            // Disable the collider to prevent further collisions
-            other.enabled = false;
-
-            // Determine the player index based on whether this player is the master client
-            int playerIndex = PhotonNetwork.IsMasterClient ? 0 : 1;
-
-            // Update the UI with the new score using ScorePlaceholder Singleton
-            ScorePlaceholder.Instance.UpdateScoreUI(score, playerIndex);
+            // Increment the score if it is me who collided with the checkpoint
+            if (photonView.IsMine)
+            {
+                score++;
+                other.enabled = false;
+            }
+            
         }
     }
 }
