9b6a5e126bce34adfccd8c755e42f56eeb01626d
beneetti <benestudium@outlook.de>
2024-08-16 17:31:59 +0200

    PU Collider

--- Assets/Scripts/CarPowerUpController.cs
+++ Assets/Scripts/CarPowerUpController.cs
@@ -2,71 +2,96 @@ using System.Collections;
 using UnityEngine;
 namespace UnityStandardAssets.Vehicles.Car
 {
-public class CarPowerUpController : MonoBehaviour
-{
-    private CarController carController;
-
-    private void Start()
+    public class CarPowerUpController : MonoBehaviour
     {
-        carController = GetComponent<CarController>();
-        if (carController == null)
+        private CarController carController;
+        private Collider carCollider;
+
+        private void Start()
         {
-            Debug.LogError("CarController component not found on this GameObject.");
+            carController = GetComponent<CarController>();
+            carCollider = GetComponent<Collider>();
+            if (carController == null)
+            {
+                Debug.LogError("CarController component not found on this GameObject.");
+            }
+            if (carCollider == null)
+            {
+                Debug.LogError("Collider component not found on this GameObject.");
+            }
         }
-    }
 
-    public void ApplySpeedBoost(float boostAmount, float duration)
-    {
-        if (carController != null)
+        public void ApplySpeedBoost(float boostAmount, float duration)
         {
-            carController.m_Topspeed += boostAmount;
-            StartCoroutine(RemoveSpeedBoostAfterTime(boostAmount, duration));
+            if (carController != null)
+            {
+                carController.m_Topspeed += boostAmount;
+                StartCoroutine(RemoveSpeedBoostAfterTime(boostAmount, duration));
+            }
         }
-    }
 
-    private IEnumerator RemoveSpeedBoostAfterTime(float boostAmount, float duration)
-    {
-        yield return new WaitForSeconds(duration);
-        if (carController != null)
+        private IEnumerator RemoveSpeedBoostAfterTime(float boostAmount, float duration)
         {
-            carController.m_Topspeed -= boostAmount;
+            yield return new WaitForSeconds(duration);
+            if (carController != null)
+            {
+                carController.m_Topspeed -= boostAmount;
+            }
         }
-    }
 
-    public void ApplyDownforceBoost(float boostAmount, float duration)
-    {
-        if (carController != null)
+        public void ApplyDownforceBoost(float boostAmount, float duration)
         {
-            carController.m_Downforce += boostAmount;
-            StartCoroutine(RemoveDownforceAfterTime(boostAmount, duration));
+            if (carController != null)
+            {
+                carController.m_Downforce += boostAmount;
+                StartCoroutine(RemoveDownforceAfterTime(boostAmount, duration));
+            }
         }
-    }
-     public void ApplyStarBoost(float amount, float duration)
-    {
-        StartCoroutine(StarBoostCoroutine(amount, duration));
-    }
 
-    private IEnumerator StarBoostCoroutine(float amount, float duration)
-    {
-        // Example implementation: temporarily increase speed and make the car invincible
-        //carController.MaxSpeed += amount;
-        // Add invincibility logic here
-
-        yield return new WaitForSeconds(duration);
-
-        //carController.MaxSpeed -= amount;
-        // Remove invincibility logic here
-    }
-
-    private IEnumerator RemoveDownforceAfterTime(float boostAmount, float duration)
-    {
-        yield return new WaitForSeconds(duration);
-        if (carController != null)
+        private IEnumerator RemoveDownforceAfterTime(float boostAmount, float duration)
         {
-            carController.m_Downforce -= boostAmount;
+            yield return new WaitForSeconds(duration);
+            if (carController != null)
+            {
+                carController.m_Downforce -= boostAmount;
+            }
         }
-    }
 
-    // More methods for other power-ups...
-}
+        public void ApplyStarBoost(float amount, float duration)
+        {
+            StartCoroutine(StarBoostCoroutine(amount, duration));
+        }
+
+        private IEnumerator StarBoostCoroutine(float amount, float duration)
+        {
+            // Temporarily increase speed
+            if (carController != null)
+            {
+                carController.m_Topspeed += amount;
+            }
+
+            // Disable the car's collider to make it invincible
+            if (carCollider != null)
+            {
+                carCollider.enabled = false;
+            }
+
+            // Wait for the duration of the power-up
+            yield return new WaitForSeconds(duration);
+
+            // Re-enable the car's collider
+            if (carCollider != null)
+            {
+                carCollider.enabled = true;
+            }
+
+            // Revert the speed boost
+            if (carController != null)
+            {
+                carController.m_Topspeed -= amount;
+            }
+        }
+
+        // More methods for other power-ups...
+    }
 }
